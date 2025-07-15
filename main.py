011d5dc0084ebfaef5a2ecaae4a56f6d332c1666
yiffery <mailyhstudios@gmail.com>
2025-07-15 10:16:57 -0400

    Title screen

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 # Python thingy majigy
-i = 2
-while True:
-  print(i**5)
-  i+=1
+
+import os
+# Callculator
+print(r"   ____      _ _            _       _             ")
+print(r"  / ___|__ _| | | ___ _   _| | __ _| |_ ___  _ __ ")
+print(r" | |   / _` | | |/ __| | | | |/ _` | __/ _ \| '__|")
+print(r" | |__| (_| | | | (__| |_| | | (_| | || (_) | |   ")
+print(r"  \____\__,_|_|_|\___|\__,_|_|\__,_|\__\___/|_|   ")
+print()
+print(" | Callculator 0.1 Alpha")
+print(" | \x1B[3mDeveloped by Yifei & Yiran\x1B[0m")
+
+os.system('clear')
